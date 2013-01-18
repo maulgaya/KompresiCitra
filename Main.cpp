0fd518fa0fb96793837a15e1628fb7fba33ac3e1
Irvan Maulana <irvan.maul@yahoo.com>
2013-01-18 19:38:22 +0700

    kompresi citra sederhana

--- Main.cpp
+++ Main.cpp
@@ -13,31 +13,13 @@ using namespace cv;
 /** @function main */
 int main( int argc, char** argv )
 {
-	IplImage* newImg = cvLoadImage("/home/maulgaya/Pictures/tes.jpg",1);
-	IplImage* grayImg = cvCreateImage( cvSize(newImg->width, newImg->height), IPL_DEPTH_8U, 1 );
-	IplImage* cannyImg = cvCreateImage(cvGetSize(newImg), IPL_DEPTH_8U, 1);
+	int p[3];
+	IplImage *img = cvLoadImage("/home/maulgaya/Pictures/tes.jpg",1);
 
-	cvCvtColor( newImg, grayImg, CV_BGR2GRAY );
+	p[0]=CV_IMWRITE_JPEG_QUALITY;
+	p[1]=10;
+	p[2]=0;
 
-	cvCanny(grayImg, cannyImg, 50, 150, 3);
-
-	cvNamedWindow("src", CV_WINDOW_NORMAL);
-	cvNamedWindow("canny", CV_WINDOW_NORMAL);
-	//compress(cannyImg);
-
-	cvShowImage( "src", newImg );
-	cvShowImage( "canny", cannyImg );
-
-	cvSaveImage("/home/maulgaya/Pictures/out1.jpg", cannyImg);
-
-	cvWaitKey(0);
-
-	cvDestroyWindow( "src" );
-	cvDestroyWindow( "canny" );
-
-	cvReleaseImage( &newImg );
-	cvReleaseImage( &grayImg );
-	cvReleaseImage( &cannyImg );
-
-	return 0;
+	cvSaveImage("/home/maulgaya/Pictures/out.jpg", img, p);
+	exit(0);
 }
